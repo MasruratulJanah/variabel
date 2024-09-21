638ac386ac65ff00ef32df020152a74bbe4cf4c7
Masruratul Janah <janahmasruratul@gmail.com>
2024-09-21 14:56:50 +0800

    Add files via upload

---
+++ Variable01.java
@@ -0,0 +1,25 @@
+public class Variable01 {
+    public static void main(String[] args) {
+        //deklarasi var
+        // kalau ada final, nilai variable menjadi konstanta
+        final String nama; //nama variable
+        int umur;
+
+        //input nilai
+        nama = "Masruratul";
+
+        //output ke layar
+        System.out.println("Nama : " + nama);
+
+        //nama = "Janah";
+        umur = 19;
+
+        //System.out.println("Nama : " + nama);
+        System.out.println("Umur : " + umur);
+
+
+
+
+
+    }
+}
